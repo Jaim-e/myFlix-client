0fb79e86a42ab3ca8a441e61035d4ff85fdf86bd
Jaim-e <51797216+Jaim-e@users.noreply.github.com>
2021-09-22 15:00:03 +0200

    Update main-view.jsx
    - Importing "axios".
    - Removing the content of "movies" array because it will be taken from my API.
    - Adding the "method": componentDidMount( ).

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -6,13 +8,21 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: "Inception", Description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.", ImagePath: "https://www.accioncine.es/images/stories/mjp/2010/08/inception_ver4.jpg", Genre:"Sci-Fi", Director: "Christopher Nolan"} ,
-        { _id: 2, Title: "The Shawshank Redemption", Description: "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.", ImagePath: "https://pics.filmaffinity.com/Cadena_perpetua-576140557-large.jpg", Genre:"Drama", Director: "Frank Darabont"},
-        { _id: 3, Title: "Gladiator", Description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", ImagePath: "https://pics.filmaffinity.com/gladiator-564554218-large.jpg", Genre:"Adventure", Director: "Ridley Scott"}
-      ],
+      movies: [],
       selectedMovie: null
     };
+  }
+
+  componentDidMount(){
+    axios.get("https://secure-coast-98530.herokuapp.com/movies")
+    .then(response => {
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -24,15 +34,14 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
 
-  //  if (selectedMovie) return <MovieView movie={selectedMovie}/>;
-
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view">Loading...</div>;
     
     return (
       <div className="main-view">
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-          : movies.map(movie => (<MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/> ))}
+          : movies.map(movie => (<MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/> ))
+        }
       </div>
     );
   }
