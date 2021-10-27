d9527b4883719b608d2dab2e9a02d496542d101f
Jaim-e <51797216+Jaim-e@users.noreply.github.com>
2021-10-27 21:42:26 +0200

    Update login-view.jsx
    Applying React-Bootstrap

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,32 +1,36 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-import "./login-view.scss";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+//import "./login-view.scss";
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState("");
   const [ password, setPassword ] = useState("");
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
+    /* Send a request to the server for authentication, then call props.onLoggedIn(username) */
     props.onLoggedIn(username);
   };
 
   return (
-    <form>
-      <label className="username">
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label className="password">
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button className="loginBtn" type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control type="text" onChange={e => setUsername(e.target.value)}></Form.Control>
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control type="password" onChange={e => setPassword(e.target.value)}></Form.Control>
+      </Form.Group>
+
+      <Button variant="primary" type="submit" onClick={handleSubmit}>Login</Button>
+    </Form>
   );
 }
 
