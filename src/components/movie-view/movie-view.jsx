8dea5bf6fe86c9521a670bf0607478b7910c639e
Jaim-e <51797216+Jaim-e@users.noreply.github.com>
2021-11-11 10:05:44 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,35 +1,43 @@
 import React from "react";
 
-export class MovieView extends React.Component {
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
+export class MovieView extends React.Component {
   render() {
     const { movie, onBackClick } = this.props;
+
     return (
-      <div className="movie-view">
+      <Row className="movie-view mt-5">
+        <Col xs={12} md={6} className="movie-poster">
+          <img src={movie.ImagePath} className="w-75" />
+        </Col>
 
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div> 
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div> 
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre}</span>
-        </div> 
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director}</span>
-        </div> 
-        <button onClick={() => { onBackClick(null); }}>Back</button>
+        <Col xs={12} md={6} className="movie-body">
+          <div className="movie-title">
+            <span className="label">Title: </span>
+            <span className="value">{movie.Title}</span>
+          </div>
 
-      </div>
+          <div className="movie-description">
+            <span className="label">Description: </span>
+            <span className="value">{movie.Description}</span>
+          </div>
+
+          <div className="movie-genre">
+            <span className="label">Genre: </span>
+            <span className="value">{movie.Genre}</span>
+          </div>
+
+          <div className="movie-director">
+            <span className="label">Director: </span>
+            <span className="value">{movie.Director}</span>
+          </div>
+
+          <Button onClick={() => { onBackClick(null); }}>Back</Button>
+        </Col>
+      </Row>
     );
   }
-
 }
