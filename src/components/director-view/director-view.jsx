fc2a336f660643fcffd28158eae09fe33b4765eb
Jaim-e <jaimemiazza@gmail.com>
2021-12-22 13:17:35 +0100

    Create director-view.jsx

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Container, Row, Button } from "react-bootstrap";
+
+import { Link } from "react-router-dom";
+
+export class DirectorView extends React.Component {
+  render() {
+    const { director} = this.props;
+
+    return (
+      <Container className="director-view mt-5">
+        <Row>
+          <h1>{director.Name}</h1>
+          <p className="director-bio">{director.Bio}</p>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="link">Back</Button>
+          </Link>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+DirectorView.propTypes = {
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string.isRequired,
+    //Birthday: PropTypes.string,
+  }).isRequired,
+};
