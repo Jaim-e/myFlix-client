e25b4eed2bf8c1b7316c5f06f20858f8a0d7f30e
Jaim-e <jaimemiazza@gmail.com>
2022-01-14 12:05:22 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Link } from "react-router-dom";
-
-import { Container, Navbar, Nav, NavItem } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 import { MainView } from "./components/main-view/main-view";
 
@@ -15,26 +13,7 @@ class MyFlixApplication extends React.Component {
   render() {
     return (
       <Container fluid xs={12} md={8}>
-
-        <Navbar bg="dark" variant="dark" expand="sm">
-          <Container fluid>
-            <Navbar.Brand href="#home">MyFlix</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav>
-                <Link to={`/users/${user}`}>
-                  <NavItem href="">Profile</NavItem>
-                </Link>
-                <Nav.Link href="#update">Update</Nav.Link>
-                <Nav.Link href="#logout">Logout</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <br />
-
         <MainView />
-
       </Container>
     );
   }
