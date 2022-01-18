1304e197ee5f4d1f77c0e17ccd05d22e11b09888
Jaim-e <jaimemiazza@gmail.com>
2022-01-18 18:13:59 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -17,17 +17,17 @@ export class MovieView extends React.Component {
 
         <Col xs={12} md={6} className="movie-body">
           <div className="movie-title">
-            <span className="label">Title: </span>
+            <span className="label"><b>Title: </b></span>
             <span className="value">{movie.Title}</span>
           </div>
 
           <div className="movie-description">
-            <span className="label">Description: </span>
+            <span className="label"><b>Description: </b></span>
             <span className="value">{movie.Description}</span>
           </div>
 
           <div className="movie-genre">
-            <span className="label">Genre: </span>
+            <span className="label"><b>Genre: </b></span>
             {/*<span className="value">{movie.Genre}</span>*/}
             <Link to={`/genres/${movie.Genre.Name}`}>
               <Button variant="link">Genre</Button>
@@ -35,7 +35,7 @@ export class MovieView extends React.Component {
           </div>
 
           <div className="movie-director">
-            <span className="label">Director: </span>
+            <span className="label"><b>Director: </b></span>
             {/*<span className="value">{movie.Director}</span>*/}
             <Link to={`/directors/${movie.Director.Name}`}>
               <Button variant="link">Director</Button>
