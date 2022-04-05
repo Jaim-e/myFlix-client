5a9658b2df5146c43b45b786017254518d717c05
Jaim-e <jaimemiazza@gmail.com>
2022-04-05 20:54:34 +0200

    Update user-info.jsx

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 
 
 function UserInfo({ name, email }) {
   return (
     <Container>
-      <h3>Your Info</h3>
+      <h3>User Info</h3>
       <br />
       <p><b>Username: </b>{name}</p>
       <p><b>E-mail: </b>{email}</p>
+      <br />
+
+      <Button variant="outline-danger" onClick={() => handleDeleteUser()}>Delete Account</Button>
+
     </Container>
   )
 }
